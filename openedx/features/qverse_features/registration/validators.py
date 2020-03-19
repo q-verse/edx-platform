e426ce41718440a484faeccf4393fff04024a8de
M Umar Khan <umar.khan@arbisoft.com>
2020-03-19 23:35:02 +0500

    Create User, UserProfile and QVerseUserProfile from uploaded csv

--- openedx/features/qverse_features/registration/validators.py
+++ openedx/features/qverse_features/registration/validators.py
@@ -1,34 +1,66 @@
 """
 Validation utils for qverse registration application.
 """
+from csv import reader, Sniffer
+import io
+
 from django.core.exceptions import ValidationError
 
 
-def validate_file_extension(file):
+def validate_admission_file(file):
     """
-    Validates that file is a valid csv file by checking its extension.
+    Validates that the file is a valid csv file by checking its extension.
+
+    Also checks that the header row has all the required fields.
+
+    Arguments:
+        file (models.FileField): The file uploaded by the user
     """
     if not file.name.endswith('.csv'):
+        raise ValidationError('', code='invalid')
+
+    FIELD_NAMES = [
+                'regno', 'firstname', 'surname', 'othername', 'levelid',
+                'programmeid', 'departmentid', 'mobile', 'email', 'mobile'
+                ]
+    decoded_file = file.read().decode('utf-8')
+    io_string = io.StringIO(decoded_file)
+    header_row = []
+    try:
+        dialect = Sniffer().sniff(io_string.readline())
+        io_string.seek(0)
+        header_row = reader(io_string, delimiter=dialect.delimiter).next()
+        header_row = [heading.lower().strip() for heading in header_row]
+    except StopIteration:
+        raise ValidationError('', code='invalid')
+
+    if not all(field_name in header_row for field_name in FIELD_NAMES):
         raise ValidationError('', code='invalid')
 
 
 def validate_current_level(value):
     """
     Validates the provided current level.
+
+    Arguments:
+        value (int): Current educational year at school
     """
     # to avoid circular import
     from openedx.features.qverse_features.registration.models import MAX_LEVEL_CHOICES
 
-    if not 1 <= value <= MAX_LEVEL_CHOICES:
+    if value not in range(1, MAX_LEVEL_CHOICES + 1):
         raise ValidationError('', code='invalid')
 
 
 def validate_programme_choices(value):
     """
     Validates the provided programme choice.
+
+    Arguments:
+        value (int): Number representing one of the programme offered by the school
     """
     # to avoid circular import
     from openedx.features.qverse_features.registration.models import MAX_PROGRAMME_CHOICES
 
-    if not 1 <= value <= MAX_PROGRAMME_CHOICES:
+    if value not in range(1, MAX_PROGRAMME_CHOICES + 1):
         raise ValidationError('', code='invalid')
