83d91b1e0478a6a4f141da7a98b6f2f97fe5ce34
M Umar Khan <umar.khan@arbisoft.com>
2020-04-22 15:08:11 +0500

    Fix value error issue and update status for prospective user creation

--- openedx/features/qverse_features/registration/signals.py
+++ openedx/features/qverse_features/registration/signals.py
@@ -10,15 +10,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from openedx.features.qverse_features.registration.models import (AdmissionData, Department, ProspectiveUser,
-                                                                  REGISTRATION_NUMBER_MAX_LENGTH, SURNAME_MAX_LENGTH,
-                                                                  FIRST_NAME_MAX_LENGTH, MOBILE_NUMBER_MAX_LENGTH,
-                                                                  OTHER_NAME_MAX_LENGTH, MAX_LEVEL_CHOICES,
-                                                                  MAX_PROGRAMME_CHOICES)
+                                                                  QVerseUserProfile, REGISTRATION_NUMBER_MAX_LENGTH,
+                                                                  SURNAME_MAX_LENGTH, FIRST_NAME_MAX_LENGTH,
+                                                                  MOBILE_NUMBER_MAX_LENGTH, OTHER_NAME_MAX_LENGTH,
+                                                                  MAX_LEVEL_CHOICES, MAX_PROGRAMME_CHOICES)
 
 
 LOGGER = logging.getLogger(__name__)
 # Prospectice User creation statuses
-PROSPECTIVE_USER_CREATED = 'Created'
+PROSPECTIVE_USER_CREATED = 'Saved'
 PROSPECTIVE_USER_UPDATED = 'Updated'
 PROSPECTIVE_USER_CREATION_FAILED = 'Failed'
 
@@ -64,6 +64,12 @@ def create_prospective_users_from_csv_file(sender, instance, created, **kwargs):
             is_valid_row, errors = _validate_single_csv_row(row)
             if is_valid_row:
                 try:
+                    if QVerseUserProfile.objects.filter(registration_number=row['regno']).exists():
+                        row['status'] = PROSPECTIVE_USER_CREATION_FAILED
+                        row['error'] = 'A user with this registration number ({}) already exists.'.format(row['regno'])
+                        output_file_rows.append(row)
+                        continue
+
                     department = Department.objects.get(number=row['departmentid'])
                     user_data = {
                         'email': row['email'],
@@ -82,8 +88,8 @@ def create_prospective_users_from_csv_file(sender, instance, created, **kwargs):
                     else:
                         row['status'] = PROSPECTIVE_USER_UPDATED
                 except (IntegrityError, Department.DoesNotExist, ValidationError) as error:
-                    LOGGER.exception('Error while creating/updating ({}) user and its '
-                                     'profiles with the following errors {}.'.format(row.get('regno'), error))
+                    LOGGER.exception('Error while creating/updating ({}) prospective user '
+                                     'with the following errors {}.'.format(row.get('regno'), error))
                     row['status'] = PROSPECTIVE_USER_CREATION_FAILED
                     if type(error) == IntegrityError:
                         row['error'] = 'Duplicate entry for email {}'.format(row['email'])
@@ -91,7 +97,7 @@ def create_prospective_users_from_csv_file(sender, instance, created, **kwargs):
                         row['error'] = error.message
 
             else:
-                LOGGER.exception('Error while creating/updating ({}) user and its profiles because of invalid values '
+                LOGGER.exception('Error while creating/updating ({}) prospective user and because of invalid values '
                                  'of required fields with the following errors ({}).'.format(row.get('regno'), errors))
                 row['status'] = PROSPECTIVE_USER_CREATION_FAILED
                 row['error'] = errors
@@ -164,11 +170,17 @@ def _validate_single_csv_row(row):
     if len(row['mobile']) > MOBILE_NUMBER_MAX_LENGTH:
         errors.append('Mobile number is more than {} characters long.'.format(MOBILE_NUMBER_MAX_LENGTH))
 
-    if int(row['levelid']) < 1 or int(row['levelid']) > MAX_LEVEL_CHOICES:
-        errors.append('Level ID must be greater than 0 and smaller than {}.'.format(MAX_LEVEL_CHOICES+1))
+    try:
+        if int(row['levelid']) < 1 or int(row['levelid']) > MAX_LEVEL_CHOICES:
+            errors.append('Level ID must be greater than 0 and smaller than {}.'.format(MAX_LEVEL_CHOICES+1))
+    except ValueError:
+        errors.append('Level ID is not an integer value.')
 
-    if int(row['programmeid']) < 1 or int(row['programmeid']) > MAX_PROGRAMME_CHOICES:
-        errors.append('Programme ID must be greater than 0 and smaller than {}.'.format(MAX_PROGRAMME_CHOICES+1))
+    try:
+        if int(row['programmeid']) < 1 or int(row['programmeid']) > MAX_PROGRAMME_CHOICES:
+            errors.append('Programme ID must be greater than 0 and smaller than {}.'.format(MAX_PROGRAMME_CHOICES+1))
+    except ValueError:
+        errors.append('Programme ID is not an integer value.')
 
     if errors:
         # to write multiple lines
